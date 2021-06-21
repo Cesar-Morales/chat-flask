cc4c6e0174339022003486f6743b242ba82f00f0
Cesar-Morales <moraleescesar@gmail.com>
2021-06-21 18:04:32 -0300

    end

--- chat-flask/main.py
+++ chat-flask/main.py
@@ -12,5 +12,10 @@ socketio = SocketIO(app)
 def index():
    return render_template('index.html')
 
+@socketio.on('message')
+def handle_message(msg):
+   print ("Message: " + msg)
+   send(msg, broadcast=True)
+
 if __name__ == '__main__':
     socketio.run(app)
